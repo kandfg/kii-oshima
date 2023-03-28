e0e31f72cd17644a0befabdf015833dc22fba0fa
kandfg <kandfg1@gmail.com>
2023-03-29 01:23:02 +0800

    push the <p> text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          這個網頁主要是展示南紀大島的日本小鍬養殖
         </p>
         <a
           className="App-link"
